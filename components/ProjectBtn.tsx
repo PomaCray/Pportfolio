cf51e0c673f5407f4423e954d5f8467bfbc0a18d
Abdurrazaq Awwal <109519925+PomaCray@users.noreply.github.com>
2024-12-27 13:17:03 +0100

    commit o

--- components/ProjectBtn.tsx
+++ components/ProjectBtn.tsx
@@ -6,10 +6,17 @@ const ProjectsBtn = () => {
   return(
 
   <div className=" bg-white/10 w-[30%] sm:w-[20%] p-3 rounded-full shadow-2xl">
-    <Link href={'#Work'} className=" flex justify-center items-center">
+    <button 
+     onClick={()=>{
+      const element = document.getElementById('Work');
+      element?.scrollIntoView({
+      behavior:'smooth'
+      })
+    }}
+    className=" flex justify-center items-center">
     <h1>Work</h1>
       <ArrowRight className="text-4xl hover:translate-x-2 transition-all duration-300"/>
-    </Link>
+    </button>
   </div>
   ) 
 };
