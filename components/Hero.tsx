825dddb49ca485c714a6b779552db89b2cb3051c
Abdurrazaq Awwal <109519925+PomaCray@users.noreply.github.com>
2025-01-22 15:01:13 +0100

    commit pfp

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ export default function Hero() {
           transition={{delay:0.5}}
           viewport={{ once: true }}
         className='absolute sm:relative top-24 sm:top-12  '>
-            <Image src={'/poma.jpg'} alt='avatar' className='object-fit rounded-full shadow-2xl h-[12rem] w-[12rem] md:h-[18rem] md:w-[18rem]' width={100} height={100}/>
+            <Image src={'/pfpme.jpg'} alt='avatar' className='object-fit rounded-full shadow-2xl h-[12rem] w-[12rem] md:h-[18rem] md:w-[18rem]' width={100} height={100}/>
         </motion.div>
 
 </motion.div> 
